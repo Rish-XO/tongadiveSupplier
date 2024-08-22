45d476f30dc9a8208c1c5d48a2923218b201dad9
Rish-XO <rishal.mhd009@gmail.com>
2024-08-22 16:50:25 +0530

    s3 done

--- app.js
+++ app.js
@@ -14,11 +14,12 @@ const PORT = process.env.PORT || 3000;
 
 const s3 = new S3Client({
   region: process.env.S3_REGION,
-  endpoint: process.env.ENDPOINT,
+  endpoint: process.env.S3_ENDPOINT,
   credentials: {
     accessKeyId: process.env.S3_ACCESS_KEY,
     secretAccessKey: process.env.S3_SECRET_KEY,
   },
+  forcePathStyle: true,
 });
 
 // MongoDB Connection
